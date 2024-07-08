99885ad7bea197a705772aa4c2fdf05ce16670eb
nhatthan2k <nhatthan0910@gmail.com>
2024-07-08 11:03:52 +0700

    add api document swagger Ui

--- src/main/java/com/appspringboot/controller/UserController.java
+++ src/main/java/com/appspringboot/controller/UserController.java
@@ -3,14 +3,14 @@ package com.appspringboot.controller;
 import com.appspringboot.configuration.Translator;
 import com.appspringboot.dto.request.UserRequestDTO;
 import com.appspringboot.dto.response.ResponseData;
-import com.appspringboot.exception.ResourceNotFoundException;
 import com.appspringboot.model.enumPackage.Gender;
 import com.appspringboot.model.enumPackage.Role;
 import com.appspringboot.model.enumPackage.UserStatus;
-import com.appspringboot.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,39 +20,51 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @Validated
+@Slf4j
+@Tag(name = "User Controller")
 public class UserController {
-    @Autowired
-    private UserService userService;
 
-    @PostMapping("")
-    public ResponseData<Integer> addUser(@RequestBody @Valid UserRequestDTO user) {
-        try {
-            userService.addUser(user);
-            return new ResponseData<>(HttpStatus.CREATED.value(), Translator.toLocale("user.add.success"), 1);
-        } catch (ResourceNotFoundException e) {
-            return new ResponseData<>(HttpStatus.BAD_REQUEST.value(), "not found name");
-        }
+    @Operation(summary = "Add User", description = "API create new user")
+    @PostMapping(value = "/")
+    public ResponseData<Integer> addUser(@Valid @RequestBody UserRequestDTO user) {
+        log.info("Request add user, {} {}", user.getFirstName(), user.getLastName());
+        return new ResponseData<>(HttpStatus.CREATED.value(), Translator.toLocale("user.add.success"), 1);
     }
 
+    @Operation(summary = "Update User", description = "API update user")
     @PutMapping("/{userId}")
-    public ResponseData<?> updateUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId, @RequestBody @Valid UserRequestDTO user) {
-        System.out.println("Update user");
-        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User updated successfully");
+    public ResponseData<?> updateUser(@PathVariable @Min(1) int userId, @Valid @RequestBody UserRequestDTO user) {
+        log.info("Request update userId={}", userId);
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), Translator.toLocale("user.upd.success"));
     }
 
+    @Operation(summary = "Update User", description = "API update user")
     @PatchMapping("/{userId}")
-    public ResponseData<?> changeUserStatus(@PathVariable int userId, @RequestParam boolean status) {
-        return new ResponseData<>(HttpStatus.ACCEPTED.value(), "User updated successfully");
+    public ResponseData<?> updateStatus(@Min(1) @PathVariable int userId, @RequestParam boolean status) {
+        log.info("Request change status, userId={}", userId);
+        return new ResponseData<>(HttpStatus.ACCEPTED.value(), Translator.toLocale("user.change.success"));
     }
 
+    @Operation(summary = "Delete User", description = "API delete user")
     @DeleteMapping("/{userId}")
-    public ResponseData<?> deleteUser(@PathVariable int userId){
-        return new ResponseData<>(HttpStatus.NO_CONTENT.value(), "User Deleted");
+    public ResponseData<?> deleteUser(@PathVariable @Min(value = 1, message = "userId must be greater than 0") int userId) {
+        log.info("Request delete userId={}", userId);
+        return new ResponseData<>(HttpStatus.NO_CONTENT.value(), Translator.toLocale("user.del.success"));
     }
 
+    @Operation(summary = "Get A User", description = "API get user detail")
     @GetMapping("/{userId}")
-    public ResponseData<List<UserRequestDTO>> getUser(@PathVariable int userId) {
-        return new ResponseData<>(HttpStatus.OK.value(), "Get successfully", List.of(new UserRequestDTO("Tay", "Java", "admin@tayjava.vn",UserStatus.ACTIVE, Gender.MALE, Role.ADMIN.toString() , "0123456789"),
-                new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", UserStatus.ACTIVE, Gender.MALE, Role.ADMIN.toString() , "0123456789")));
+    public ResponseData<UserRequestDTO> getUser(@PathVariable @Min(1) int userId) {
+        log.info("Request get user detail, userId={}", userId);
+        return new ResponseData<>(HttpStatus.OK.value(), "user", new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", UserStatus.ACTIVE, Gender.MALE, Role.ADMIN.toString(),"0123456789"));
+    }
+
+    @Operation(summary = "Get user list per page", description = "API return users by pageNo and pageSize")
+    @GetMapping("/list")
+    public ResponseData<List<UserRequestDTO>> getAllUser(@RequestParam(defaultValue = "0", required = false) int pageNo,
+                                                         @Min(10) @RequestParam(defaultValue = "20", required = false) int pageSize) {
+        log.info("Request get all of users");
+        return new ResponseData<>(HttpStatus.OK.value(), "users", List.of(new UserRequestDTO("Tay", "Java", "admin@tayjava.vn", UserStatus.ACTIVE, Gender.MALE, Role.ADMIN.toString(), "0123456789"),
+                new UserRequestDTO("Leo", "Messi", "leomessi@email.com", UserStatus.ACTIVE, Gender.MALE, Role.ADMIN.toString(),"0123456456")));
     }
 }
